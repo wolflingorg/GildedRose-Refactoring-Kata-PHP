2b46f196cae233f100649e9f952c479f4cb9a039
Maksym Odanets <Maksym_Odanets@epam.com>
2018-05-12 15:10:12 +0300

    Get rid of "else" statement

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -7,6 +7,10 @@
  */
 class GildedRose
 {
+    const AGED_BRIE = 'Aged Brie';
+    const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+
     /**
      * @var Item[]
      */
@@ -15,57 +19,63 @@ class GildedRose
     /**
      * @param $items
      */
-    public function __construct(array $items) {
+    public function __construct(array $items)
+    {
         $this->items = $items;
     }
 
     /**
      * Updates quality and sellIn for Item
      */
-    public function updateQuality() {
+    public function updateQuality()
+    {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
+            if ($item->name == self::AGED_BRIE) {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                }
+            }
+
+            if ($item->name == self::BACKSTAGE) {
+                if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
+                    if ($item->sellIn < 11) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                    }
+                    if ($item->sellIn < 6) {
+                        if ($item->quality < 50) {
+                            $item->quality = $item->quality + 1;
                         }
                     }
                 }
             }
-            
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+
+            if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
+                if ($item->quality > 0) {
+                    $item->quality = $item->quality - 1;
+                }
+            }
+
+            if ($item->name != self::SULFURAS) {
                 $item->sellIn = $item->sellIn - 1;
             }
-            
+
             if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
+                if ($item->name == self::AGED_BRIE) {
                     if ($item->quality < 50) {
                         $item->quality = $item->quality + 1;
+                    }
+                }
+
+                if ($item->name == self::BACKSTAGE) {
+                    $item->quality = $item->quality - $item->quality;
+                }
+
+                if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
+                    if ($item->quality > 0) {
+                        $item->quality = $item->quality - 1;
                     }
                 }
             }
