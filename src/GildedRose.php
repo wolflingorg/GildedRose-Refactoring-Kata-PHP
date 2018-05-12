29fc8ae9d083726df130b284c1e723ccc383aea3
Maksym Odanets <Maksym_Odanets@epam.com>
2018-05-12 15:43:50 +0300

    Extract method -> for processing single Item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,12 +24,56 @@ class GildedRose
         $this->items = $items;
     }
 
+
+
     /**
      * Updates quality and sellIn for Item
      */
     public function updateQuality()
     {
         foreach ($this->items as $item) {
+            $this->processItem($item);
+        }
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function processItem(Item $item)
+    {
+        if ($item->name == self::AGED_BRIE) {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+            }
+        }
+
+        if ($item->name == self::BACKSTAGE) {
+            if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->sellIn < 11) {
+                    if ($item->quality < 50) {
+                        $item->quality = $item->quality + 1;
+                    }
+                }
+                if ($item->sellIn < 6) {
+                    if ($item->quality < 50) {
+                        $item->quality = $item->quality + 1;
+                    }
+                }
+            }
+        }
+
+        if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
+            }
+        }
+
+        if ($item->name != self::SULFURAS) {
+            $item->sellIn = $item->sellIn - 1;
+        }
+
+        if ($item->sellIn < 0) {
             if ($item->name == self::AGED_BRIE) {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
@@ -37,46 +81,12 @@ class GildedRose
             }
 
             if ($item->name == self::BACKSTAGE) {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->sellIn < 11) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                    if ($item->sellIn < 6) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
+                $item->quality = $item->quality - $item->quality;
             }
 
             if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
                 if ($item->quality > 0) {
                     $item->quality = $item->quality - 1;
-                }
-            }
-
-            if ($item->name != self::SULFURAS) {
-                $item->sellIn = $item->sellIn - 1;
-            }
-
-            if ($item->sellIn < 0) {
-                if ($item->name == self::AGED_BRIE) {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
-
-                if ($item->name == self::BACKSTAGE) {
-                    $item->quality = $item->quality - $item->quality;
-                }
-
-                if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
-                    if ($item->quality > 0) {
-                        $item->quality = $item->quality - 1;
-                    }
                 }
             }
         }
