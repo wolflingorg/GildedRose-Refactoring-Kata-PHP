74e0b22de46156c0bab6f7a8b505db26d3df7390
Maksym Odanets <Maksym_Odanets@epam.com>
2018-05-12 15:48:39 +0300

    Extract method -> for processing Normal Item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -10,6 +10,7 @@ class GildedRose
     const AGED_BRIE = 'Aged Brie';
     const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
     const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+    const NORMAL = 'Normal';
 
     /**
      * @var Item[]
@@ -41,6 +42,11 @@ class GildedRose
      */
     private function processItem(Item $item)
     {
+        if ($item->name == self::NORMAL) {
+            $this->processNormalItem($item);
+            return;
+        }
+
         if ($item->name == self::AGED_BRIE) {
             if ($item->quality < 50) {
                 $item->quality = $item->quality + 1;
@@ -63,12 +69,6 @@ class GildedRose
             }
         }
 
-        if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
-            if ($item->quality > 0) {
-                $item->quality = $item->quality - 1;
-            }
-        }
-
         if ($item->name != self::SULFURAS) {
             $item->sellIn = $item->sellIn - 1;
         }
@@ -83,12 +83,21 @@ class GildedRose
             if ($item->name == self::BACKSTAGE) {
                 $item->quality = $item->quality - $item->quality;
             }
-
-            if ($item->name != self::AGED_BRIE && $item->name != self::BACKSTAGE && $item->name != self::SULFURAS) {
-                if ($item->quality > 0) {
-                    $item->quality = $item->quality - 1;
-                }
-            }
         }
+    }
+
+    /**
+     * @param Item $item
+     */
+    private function processNormalItem(Item $item)
+    {
+        $item->sellIn -= 1;
+        $item->quality -= 1;
+
+        if ($item->sellIn < 0) {
+            $item->quality -= 1;
+        }
+
+        $item->quality = ($item->quality >= 0) ? $item->quality : 0;
     }
 }
