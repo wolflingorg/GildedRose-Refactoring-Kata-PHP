5e41345be2a9d67377b5e7561d2964042bbc9cc6
Maksym Odanets <Maksym_Odanets@epam.com>
2018-05-12 23:13:10 +0300

    Normal Item

--- tests/ItemFactoryTest.php
+++ tests/ItemFactoryTest.php
@@ -13,7 +13,7 @@ class ItemFactoryTest extends TestCase
             [["Sulfuras, Hand of Ragnaros", 0, 0], SulfurasItem::class],
             [["Aged Brie", 0, 0], AgedBrieItem::class],
             [["Backstage passes to a TAFKAL80ETC concert", 0, 0], BackstageItem::class],
-            [["Normal", 0, 0], AbstractItem::class],
+            [["Normal", 0, 0], NormalItem::class],
         ];
     }
 
